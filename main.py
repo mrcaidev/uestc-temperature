0123caa1275cf337778e0c539ae4a22e316dde01
蔡与望 <78269445+MrCaiDev@users.noreply.github.com>
2021-09-08 05:41:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Report:
 
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.38',
+            'Referer': 'https://servicewechat.com/wx521c0c16b77041a0/28/page-frame.html',
             'cookie': None
         }
 
