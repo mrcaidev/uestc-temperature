5ca9d043b256d2fcb7cfc37afcddc9fa54d0f694
蔡与望 <78269445+MrCaiDev@users.noreply.github.com>
2021-09-08 05:50:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class Report:
         self.cookies = sys.argv[1].split('#')
 
         self.headers = {
+            'content-type': 'application/json',
+            'encode': 'false',
+            'Connection': 'keep-alive',
+            'x-tag': 'flyio',
+            'charset': 'utf-8',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.38',
             'Referer': 'https://servicewechat.com/wx521c0c16b77041a0/28/page-frame.html',
             'cookie': None
