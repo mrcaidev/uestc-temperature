dedac5ece5ca6c24f9eb5ff8f4a93004b9b6ccd9
mrcaidev <mrcaidev@qq.com>
2022-02-05 14:45:30 +0800

    :art: 使用 black 格式化代码

--- src/main.py
+++ src/main.py
@@ -4,20 +4,20 @@ import time
 from reporter import Reporter
 
 if __name__ == "__main__":
-    print(f'[{time.strftime("%F %H:%M:%S")}]', end=' ')
+    print(f'[{time.strftime("%F %H:%M:%S")}]', end=" ")
 
-    cookies = os.environ.get('COOKIES')
+    cookies = os.environ.get("COOKIES")
     if cookies == None:
-        raise Exception('session id not provided')
+        raise Exception("session id not provided")
     else:
-        cookies = cookies.split('#')
+        cookies = cookies.split("#")
 
     results = []
     for index, cookie in enumerate(cookies):
         reporter = Reporter(cookie)
         result, message = reporter.run()
         results.append(result)
-        print(f'Student {index+1}: {message}')
+        print(f"Student {index+1}: {message}")
 
     if not all(results):
         exit(-1)
